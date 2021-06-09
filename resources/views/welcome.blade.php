7fae564df874d498cb45093758f44fecc161fd44
psmever <psmever@gmail.com>
2021-06-09 18:29:56 +0900

    [UPDAETE] landding page.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Freelancer - Start Bootstrap Theme</title>
+    <title>lunatalk backend server</title>
     <!-- Favicon-->
     <link rel="icon" type="image/x-icon" href="{{URL::asset('assets/favicon.ico')}}" />
     <!-- Font Awesome icons (free version)-->
