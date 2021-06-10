6420b6d22333b1b39c13b4c4b09c78fd41cda321
psmever <psmever@gmail.com>
2021-06-10 14:36:30 +0900

    [ADD] fileststems

--- config/filesystems.php
+++ config/filesystems.php
@@ -52,6 +52,17 @@ return [
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
         ],
+        /**
+         * Custom Setting
+         */
+        'inside-space' => [
+            'driver' => 'local',
+            'root' => storage_path('inside/space'),
+        ],
+        'inside-temp' => [
+            'driver' => 'local',
+            'root' => storage_path('inside/temp'),
+        ],
 
     ],
 
