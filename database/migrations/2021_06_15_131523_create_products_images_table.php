29282c94e11861c7341ca73d6fb7c8b9e1655c5b
psmever <psmever@gmail.com>
2021-06-15 15:16:05 +0900

    [UPDATE] migration.

--- database/migrations/2021_06_15_131523_create_products_images_table.php
+++ database/migrations/2021_06_15_131523_create_products_images_table.php
@@ -16,11 +16,18 @@ class CreateProductsImagesTable extends Migration
         Schema::create('products_images', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('product_id')->nullable(false)->index()->comment('상품 고유값.');
-            $table->unsignedBigInteger('media_file_id')->nullable()->comment('media file table id.');
+
+            $table->unsignedBigInteger('product_image')->nullable()->comment('제품 이미지.');
+            $table->unsignedBigInteger('product_thumbnail_image')->nullable()->comment('제품 썸네일 이미지.');
+            $table->unsignedBigInteger('product_detail_image')->nullable()->comment('제품 상세 이미지.');
+
             $table->timestamps();
-            $table->index(['product_id', 'media_file_id']);
+
             $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
-            $table->foreign('media_file_id')->references('id')->on('media_files')->onDelete('cascade');
+
+            $table->foreign('product_image')->references('id')->on('media_files')->onDelete('cascade');
+            $table->foreign('product_thumbnail_image')->references('id')->on('media_files')->onDelete('cascade');
+            $table->foreign('product_detail_image')->references('id')->on('media_files')->onDelete('cascade');
         });
     }
 
