81b5dea11caf745ff2e4fa166293cb90d0374506
psmever <psmever@gmail.com>
2021-08-09 07:35:52 +0900

    [Working] login.

--- database/migrations/2021_06_10_172619_update_users_table.php
+++ database/migrations/2021_06_10_172619_update_users_table.php
@@ -19,7 +19,6 @@ class UpdateUsersTable extends Migration
             $table->char('user_type', 7)->after('user_uuid')->default(config('extract.clientType.front.code'))->comment('회원 타입');
             $table->char('user_level', 7)->after('user_type')->default(config('extract.user.user_level.user.level_code'))->comment('회원 레벨');
             $table->enum('active', ['Y', 'N'])->after('remember_token')->default('Y')->comment('회원 상태');
-            $table->string('nickname', 50)->after('name')->default('')->comment('회원 닉네임');
 
             $table->foreign('user_type')->references('code_id')->on('codes')->onDelete('cascade');
             $table->foreign('user_level')->references('code_id')->on('codes')->onDelete('cascade');
@@ -36,7 +35,7 @@ class UpdateUsersTable extends Migration
         Schema::table('users', function (Blueprint $table) {
             if(DB::getDriverName() !== 'sqlite') $table->dropForeign('users_user_level_foreign');
             if(DB::getDriverName() !== 'sqlite') $table->dropForeign('users_user_type_foreign');
-            if(DB::getDriverName() !== 'sqlite') $table->dropColumn(['user_uuid', 'user_type', 'user_level', 'active', 'nickname']);
+            if(DB::getDriverName() !== 'sqlite') $table->dropColumn(['user_uuid', 'user_type', 'user_level', 'active']);
         });
     }
 }
