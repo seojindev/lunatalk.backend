15ed98f6b287c597846e697a8958cfee6aa7c3c3
psmever <psmever@gmail.com>
2021-09-11 17:39:52 +0900

    [ADD] product delete unit test.

--- tests/Feature/Api/Admin/v1/ProductController/ProductDetailTest.php
+++ tests/Feature/Api/Admin/v1/ProductController/ProductDetailTest.php
@@ -2,11 +2,14 @@
 
 namespace Tests\Feature\Api\Admin\v1\ProductController;
 
-use App\Models\ProductCategoryMasters;
+use App\Models\ProductImages;
+use App\Models\ProductMasters;
+use App\Models\ProductOptions;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\DB;
 use Tests\BaseCustomTestCase;
 
-class CategoryDetailTest extends BaseCustomTestCase
+class productDetailTest extends BaseCustomTestCase
 {
     protected string $apiURL;
 
@@ -14,19 +17,20 @@ class CategoryDetailTest extends BaseCustomTestCase
     {
         parent::setUp();
 
-        $this->apiURL = "/api/admin-front/v1/product/:uuid:/detail-product-category";
+        $this->apiURL = "/api/admin-front/v1/product/:uuid:/delete-product";
     }
 
-    public function test_admin_front_v1_product_category_detail_존재하지_않은_uuid_요청()
+    public function test_admin_front_v1_product_delete_존재하지_않은_uuid_요청()
     {
         $this->expectException(ModelNotFoundException::class);
 
         $this->withHeaders($this->getTestAdminAccessTokenHeader())->json('GET', str_replace(':uuid:', 'asdasdasdasdasdasd', $this->apiURL));
     }
 
-    public function test_admin_front_v1_product_detail_정상_요청()
+    public function test_admin_front_v1_product_delete_정상_요청()
     {
-        $uuid = ProductCategoryMasters::select('uuid')->inRandomOrder()->first()->uuid;
+        $productData = $this->insertTestProductMaster();
+        $uuid = ProductMasters::select('uuid')->where('id', $productData->id)->first()->uuid;
 
         $response = $this->withHeaders($this->getTestAdminAccessTokenHeader())->json('GET', str_replace(':uuid:', $uuid, $this->apiURL));
         $response->assertStatus(200);
@@ -37,6 +41,11 @@ class CategoryDetailTest extends BaseCustomTestCase
                 'name'
             ]
         ]);
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        ProductImages::truncate();
+        ProductOptions::truncate();
+        ProductMasters::truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
-
