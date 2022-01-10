e21055f8255fb8fab7cc550badd11521e78d9d71
psmever <psmever@gmail.com>
2022-01-10 22:08:45 +0900

    [UPDATE] state column.

---
+++ database/migrations/2022_01_10_220558_add_order_master_table_state_column.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddOrderMasterTableStateColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('order_masters', function (Blueprint $table) {
+            $table->enum('state',['Y','N'])->after('active')->default('N')->comment('결제 상.');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('order_masters', function (Blueprint $table) {
+            $table->dropColumn(['state']);
+        });
+    }
+}
