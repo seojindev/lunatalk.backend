664484fcc40641c47e21822fb388ccf48673e6df
kkwondev <kkwoncokr@gmail.com>
2022-03-25 00:02:09 +0900

    [ADD] 아이디 찾기

---
+++ app/Mail/sendMail.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class sendMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public Array $details;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($details)
+    {
+        $this->details = $details;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('루나톡 아이디 찾기 lunatalk.co.kr')->view('email.idPasswordEmail');
+    }
+}
