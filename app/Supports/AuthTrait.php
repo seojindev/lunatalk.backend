512405edaabf97b7f0c3f76d87e5bc9fcd660639
kkwondev <kkwoncokr@gmail.com>
2022-06-25 09:40:48 +0900

    [ADD] 전화번호 추가

--- app/Supports/AuthTrait.php
+++ app/Supports/AuthTrait.php
@@ -14,39 +14,12 @@ trait AuthTrait
      */
     public function sendAuthSMS(string $phoneNumber, string $message)
     {
-        $sendPhoneNumbers =explode(',', env('SMS_SEND_NO'));
-        foreach($sendPhoneNumbers as $adminPhoneNumber) {
-            $response = Http::withHeaders(
-                [
-                    'X-Secret-Key' => env('SMS_SECRET_KEY'),
-                    'Content-Type' => 'application/json;charset=UTF-8'
-                ]
-            )->post('https://api-sms.cloud.toast.com/sms/v3.0/appKeys/'.env('SMS_API_KEY').'/sender/auth/sms',
-                [
-                    'body' => $message,
-                    'sendNo' => $adminPhoneNumber,
-                    'recipientList' => [array('recipientNo' => $phoneNumber, 'countryCode' => '82')]
-                ]);
-
-            $responseStatusCode = $response->json();
-            $resultCode = $responseStatusCode['body']['data']['sendResultList'][0]['resultCode'];
-            if($resultCode != 0) {
-                if($resultCode == -2019) {
-                    throw new ClientErrorException(__('register.phone_auth_confirm.message_server_number_not_valid'));
-                }
-                throw new ServerErrorException(__('register.phone_auth_confirm.message_server_error'));
-            }
-        }
-    }
-
-    public function sendSMS(string $phoneNumber, string $message)
-    {
         $response = Http::withHeaders(
             [
                 'X-Secret-Key' => env('SMS_SECRET_KEY'),
                 'Content-Type' => 'application/json;charset=UTF-8'
             ]
-        )->post('https://api-sms.cloud.toast.com/sms/v3.0/appKeys/'.env('SMS_API_KEY').'/sender/sms',
+        )->post('https://api-sms.cloud.toast.com/sms/v3.0/appKeys/'.env('SMS_API_KEY').'/sender/auth/sms',
             [
                 'body' => $message,
                 'sendNo' =>env('SMS_SEND_NO'),
@@ -60,6 +33,33 @@ trait AuthTrait
                 throw new ClientErrorException(__('register.phone_auth_confirm.message_server_number_not_valid'));
             }
             throw new ServerErrorException(__('register.phone_auth_confirm.message_server_error'));
+        }
+    }
+
+    public function sendSMS(string $phoneNumber, string $message)
+    {
+        $sendPhoneNumbers = explode(',', env('SMS_SEND_NO'));
+        foreach ($sendPhoneNumbers as $adminPhoneNumber) {
+            $response = Http::withHeaders(
+                [
+                    'X-Secret-Key' => env('SMS_SECRET_KEY'),
+                    'Content-Type' => 'application/json;charset=UTF-8'
+                ]
+            )->post('https://api-sms.cloud.toast.com/sms/v3.0/appKeys/'.env('SMS_API_KEY').'/sender/sms',
+                [
+                    'body' => $message,
+                    'sendNo' =>$adminPhoneNumber,
+                    'recipientList' => [array('recipientNo' => $phoneNumber, 'countryCode' => '82')]
+                ]);
+
+            $responseStatusCode = $response->json();
+            $resultCode = $responseStatusCode['body']['data']['sendResultList'][0]['resultCode'];
+            if($resultCode != 0) {
+                if($resultCode == -2019) {
+                    throw new ClientErrorException(__('register.phone_auth_confirm.message_server_number_not_valid'));
+                }
+                throw new ServerErrorException(__('register.phone_auth_confirm.message_server_error'));
+            }
         }
     }
 }
