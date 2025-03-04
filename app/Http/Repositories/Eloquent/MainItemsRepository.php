a57b12716fb6fa6f55ad3c13d071014851d7a492
Theo <kkw.theo@gmail.com>
2025-03-04 11:38:06 +0900

    Revert "상품 활성화 된것만 랜덤으로 뽑도록 처리"
    This reverts commit 461cfa20be4d3e362571c31505fe35a268da724b.

--- app/Http/Repositories/Eloquent/MainItemsRepository.php
+++ app/Http/Repositories/Eloquent/MainItemsRepository.php
@@ -90,9 +90,7 @@ class MainItemsRepository extends BaseRepository implements MainItemsRepositoryI
      */
     public function getFrontMainBestItems() : Collection {
         return $this->model->where('category', config('extract.main_item.bestItem.code'))
-            ->with(['product.repImage.image', 'product.colors.color', 'product.badge.badge.image', 'product.reviews', 'product' => function($query) {
-                $query->where('active', 'N')
-            }])
+            ->with(['product.repImage.image', 'product.colors.color', 'product.badge.badge.image', 'product.reviews'])
             ->inRandomOrder()
             ->get();
     }
@@ -104,9 +102,7 @@ class MainItemsRepository extends BaseRepository implements MainItemsRepositoryI
     public function getFrontMainNewItems() : Collection {
         return $this->model->where('category', config('extract.main_item.newItem.code'))
 //            ->where('active', 'Y')
-            ->with(['product.repImage.image', 'product.colors.color', 'product.badge.badge.image', 'product.reviews','product' => function($query) {
-                $query->where('active', 'N')
-            }])
+            ->with(['product.repImage.image', 'product.colors.color', 'product.badge.badge.image', 'product.reviews'])
             ->inRandomOrder()
             ->get();
     }
